36961a3546a26c1dfcb60c55061a20e751f59589
Marco-Senpai <mcneilmarcus92@gmail.com>
2019-05-23 21:40:31 -0500

    another draft

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,20 +6,81 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:swiss@localhost:3306/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-# app.secret_key = 'y2k'             
+app.secret_key = 'y2k'             
+
 
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(265))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
-
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
 
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password, blogs):
+        self.username = username
+        self.password = password
+        self.blogs = blogs
+
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('login')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            flash("Logged In")
+            return redirect('/newpost')
+        else:
+            flash('User password incorrect, or User does not exist', 'error')
+
+    return render_template('blogs.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+        else: 
+            return "<h1>Duplicate User<h1>"
+
+    return render_template('newpost.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    del session['username']
+    return redirect('/')
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
@@ -40,31 +101,42 @@ def new_post():
             new_blog = Blog(blog_title, blog_body)
             db.session.add(new_blog)
             db.session.commit()
-            # cheese = "/blog?id=" + str(new_blog.id)
-            # return redirect(cheese)
-            
+            cheese = "/blog?id=" + str(new_blog.id)
+            return redirect(cheese)
         else:
             return render_template('newpost.html', 
-        title="Add Blog Entry", title_error=title_error, 
-        body_error=body_error)
+            title="Add Blog Entry", title_error=title_error, 
+            body_error=body_error)
 
 
 
 @app.route('/blog', methods=['GET'])
 def blog():
     if request.args:
-        blog_id = request.args("id")
+        blog_id = request.args.get("id")
         blog = Blog.query.get(blog_id)
-        return render_template('blogs.html', blog=blog)
+        return render_template('display.html', blog=blog)
     else:
         blogs = Blog.query.all()
         return render_template('blogs.html', title="Build a Blog", blogs=blogs)
         
+
 @app.route('/', methods=['GET'])
 def index():
     if request.method == 'GET':
         cheese = "/blog?id=" + str(new_blog.id)
         return redirect(cheese)
+
+@app.route('/delete-blog', methods=['blog_id'])
+def delete_blog():
+
+    blog_id = int(request.form['blog_id'])
+    blog = blog.query.get(blog_id)
+    db.session.add(blog)
+    db.session.commit()
+    cheese = "/blog?id=" + str(new_blog.id)
+    return redirect(cheese)
+
 
 if __name__ == '__main__':
     app.run()
