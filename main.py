b942da35d299ff68416777bf564b0c4aa61c2402
Marco-Senpai <mcneilmarcus92@gmail.com>
2019-05-25 09:46:40 -0500

    3rd draft

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ class Blog(db.Model):
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(20))
     blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password):
@@ -34,12 +34,11 @@ class User(db.Model):
         self.password = password
        
 
-
-@app.before_request
-def require_login():
-    allowed_routes = ['login', 'signup', 'blog', '/']
-    if request.endpoint not in allowed_routes and 'username' not in session:
-        return redirect('login')
+# @app.before_request
+# def require_login():
+#     allowed_routes = ['login', 'signup', 'blog', '/']
+#     if request.endpoint not in allowed_routes and 'username' not in session:
+#         return redirect('login')
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -50,12 +49,12 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
             session['username'] = username
-            flash("Logged In")
+            flash("You're Logged In")
             return redirect('/newpost')
         else:
-            flash('User password incorrect, or User does not exist', 'error')
+            flash('User or password is incorrect, or User does not exist', 'error')
 
-    return render_template('blogs.html')
+    return render_template('login.html')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -64,50 +63,60 @@ def signup():
         username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
+        if len(username) < 3 or len(username) > 20 or " " in username:
+            flash('Incorrect username', 'error')
+        elif len(password) < 3 or len(password) > 20 or " " in password:
+            flash('Incorrect password', 'error')
+        elif password != verify:
+            flash('passwords do not match', 'error')
+        else:
+            existing_user = User.query.filter_by(username=username).first()
+            if existing_user:
+                flash("Duplicate User", 'error')
+            else:
+                new_user = User(username, password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['username'] = username
+                return redirect('/newpost')
 
-        existing_user = User.query.filter_by(username=username).first()
-        if not existing_user:
-            new_user = User(username, password)
-            db.session.add(new_user)
-            db.session.commit()
-            session['username'] = username
-            return redirect('/newpost')
-        else: 
-            return "<h1>Duplicate User<h1>"
+    return render_template('signup.html')
 
-    return render_template('newpost.html')
 
-@app.route('/logout', methods=['GET'])
+@app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    del session['username']
-    return redirect('/')
+    if 'username' in session:
+        del session['username']
+        return redirect('/')
+    return redirect('/signup')
 
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
+    owner = User.query.filter_by(username=session['username']).first()
     if request.method == 'GET':
-        return render_template('newpost.html', 
-        title="Add Blog Entry")
+        return render_template('newpost.html', title="New Post")
+    
     if request.method == 'POST':    
         blog_title = request.form['blog_title']
         blog_body = request.form['blog_body']
         title_error = ''
         body_error = ''
+        
         if len(blog_title) < 1:
             title_error = "please add an entry"
         if len(blog_body) < 1:
             body_error = "please add a body"
         if not title_error and not body_error:
-            new_blog = Blog(blog_title, blog_body)
+            new_blog = Blog(blog_title, blog_body, owner)
             db.session.add(new_blog)
             db.session.commit()
-            cheese = "/blog?id=" + str(new_blog.id)
-            return redirect(cheese)
+            # cheese = "/blog?id=" + str(new_blog.id)
+            return render_template('blog.html')
         else:
             return render_template('newpost.html', 
-            title="Add Blog Entry", title_error=title_error, 
-            body_error=body_error)
-
+        title="New Post", title_error=title_error, 
+        body_error=body_error)
 
 
 @app.route('/blog', methods=['GET'])
@@ -120,10 +129,10 @@ def blog():
         user_id = request.args.get("user")
         user = User.query.get(user_id)
         blogs = Blog.query.filter_by(owner=user)
-        return render_template('singleUser.html', blogs=blogs)
+        return render_template('singleUser.html')
     else:
         blogs = Blog.query.all()
-        return render_template('blogs.html', title="Blogz", blogs=blogs)
+        return render_template('blog.html', title="Blogz", blogs=blogs)
         
 
 @app.route('/', methods=['GET'])
@@ -132,6 +141,9 @@ def index():
     return render_template('index.html', title="Blogz", users=users)
 
 
+@app.route('/singleUser', methods=['GET'])
+def singleUser():
+    return render_template('/')
 # @app.route('/delete-blog', methods=['blog_id'])
 # def delete_blog():
 
